802f3e39906100aeb521f10d09fa64cf1b13ca9a
moinuddin <moinuddinchudiwal2021@gmail.com>
2023-05-20 15:17:15 +0530

    Categories UI completed

--- src/schemas/AddCategorySchema.tsx
+++ src/schemas/AddCategorySchema.tsx
@@ -3,5 +3,8 @@ import * as Yup from 'yup';
 
 export const addCategorySchema  = Yup.object({
     name: Yup.string().required('Please enter category name'),
+    status : Yup.string().required('Please select category status'),
+    created_at : Yup.date().required('Please select a created date').nullable(),
+    updated_at : Yup.date().required('Please select a updated date').nullable(),
     img_url: Yup.mixed().required('Please choose category image'),
 })
